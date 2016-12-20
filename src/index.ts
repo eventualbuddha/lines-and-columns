7fc83a70df2688003ccaa70d0f4f431fc7f698d9
Brian Donovan <me@brian-donovan.com>
2016-12-20 09:13:05 -0800

    refactor: switch from flow to TypeScript

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
-/* @flow */
-
-type SourceLocation = {
+export type SourceLocation = {
   line: number,
   column: number
 };
@@ -9,11 +7,11 @@ const LF = '\n';
 const CR = '\r';
 
 export default class LinesAndColumns {
-  _string: string;
-  _offsets: Array<number>;
+  private string: string;
+  private offsets: Array<number>;
 
   constructor(string: string) {
-    this._string = string;
+    this.string = string;
 
     let offsets = [0];
 
@@ -38,45 +36,42 @@ export default class LinesAndColumns {
       }
     }
 
-    this._offsets = offsets;
+    this.offsets = offsets;
   }
 
-  locationForIndex(index: number): ?SourceLocation {
-    if (index < 0 || index > this._string.length) {
+  locationForIndex(index: number): SourceLocation | null {
+    if (index < 0 || index > this.string.length) {
       return null;
     }
 
     let line = 0;
-    let offsets = this._offsets;
+    let offsets = this.offsets;
 
     while (offsets[line + 1] <= index) {
       line++;
     }
 
     let column = index - offsets[line];
-    return ({ line, column }: SourceLocation);
+    return { line, column };
   }
 
-  indexForLocation(location: SourceLocation): ?number {
+  indexForLocation(location: SourceLocation): number | null {
     let { line, column } = location;
 
-    if (line < 0 || line >= this._offsets.length) {
+    if (line < 0 || line >= this.offsets.length) {
       return null;
     }
 
-    if (column < 0 || column > this._lengthOfLine(line)) {
+    if (column < 0 || column > this.lengthOfLine(line)) {
       return null;
     }
 
-    return this._offsets[line] + column;
+    return this.offsets[line] + column;
   }
 
-  /**
-   * @private
-   */
-  _lengthOfLine(line: number): number {
-    let offset = this._offsets[line];
-    let nextOffset = line === this._offsets.length - 1 ? this._string.length : this._offsets[line + 1];
+  private lengthOfLine(line: number): number {
+    let offset = this.offsets[line];
+    let nextOffset = line === this.offsets.length - 1 ? this.string.length : this.offsets[line + 1];
     return nextOffset - offset;
   }
 }
