ab5fa220516b298726f0826b5f64715ee789c537
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2021-11-20 13:12:57 -0800

    build: switch to ES modules
    This is a BREAKING CHANGE. Also switched to a named export.
    
    Closes #198

--- test/test.ts
+++ test/test.ts
@@ -1,96 +1,93 @@
-import { deepEqual as deq, strictEqual as eq } from 'assert';
-import LinesAndColumns from '../src';
+import { LinesAndColumns } from '../src'
 
-describe('LinesAndColumns', function() {
-  it('maps index 0 to line 0, column 0', function() {
-    const map = new LinesAndColumns('a');
-    deq(map.locationForIndex(0), { line: 0, column: 0 });
-  });
+test('maps index 0 to line 0, column 0', function () {
+  const map = new LinesAndColumns('a')
+  expect(map.locationForIndex(0)).toEqual({ line: 0, column: 0 })
+})
 
-  it('maps indexes in a single-line string to line 0 and column at index', function() {
-    const map = new LinesAndColumns('abcd');
-    deq(map.locationForIndex(2), { line: 0, column: 2 });
-  });
+test('maps indexes in a single-line string to line 0 and column at index', function () {
+  const map = new LinesAndColumns('abcd')
+  expect(map.locationForIndex(2)).toEqual({ line: 0, column: 2 })
+})
 
-  it('maps indexes in a multi-line string to the appropriate line and column', function() {
-    const map = new LinesAndColumns('ab\ncd');
-    deq(map.locationForIndex(2), { line: 0, column: 2 });
-    deq(map.locationForIndex(3), { line: 1, column: 0 });
-  });
+test('maps indexes in a multi-line string to the appropriate line and column', function () {
+  const map = new LinesAndColumns('ab\ncd')
+  expect(map.locationForIndex(2)).toEqual({ line: 0, column: 2 })
+  expect(map.locationForIndex(3)).toEqual({ line: 1, column: 0 })
+})
 
-  it('maps indexes before the beginning to null', function() {
-    const map = new LinesAndColumns('');
-    eq(map.locationForIndex(-1), null);
-  });
+test('maps indexes before the beginning to null', function () {
+  const map = new LinesAndColumns('')
+  expect(map.locationForIndex(-1)).toEqual(null)
+})
 
-  it('maps indexes at the end', function() {
-    const map = new LinesAndColumns('');
-    deq(map.locationForIndex(0), { line: 0, column: 0 });
-  });
+test('maps indexes at the end', function () {
+  const map = new LinesAndColumns('')
+  expect(map.locationForIndex(0)).toEqual({ line: 0, column: 0 })
+})
 
-  it('maps indexes after the end to null', function() {
-    const map = new LinesAndColumns('');
-    eq(map.locationForIndex(1), null);
-  });
+test('maps indexes after the end to null', function () {
+  const map = new LinesAndColumns('')
+  expect(map.locationForIndex(1)).toEqual(null)
+})
 
-  it('maps line 0, column 0 to index 0', function() {
-    const map = new LinesAndColumns('a');
-    eq(map.indexForLocation({ line: 0, column: 0 }), 0);
-  });
+test('maps line 0, column 0 to index 0', function () {
+  const map = new LinesAndColumns('a')
+  expect(map.indexForLocation({ line: 0, column: 0 })).toEqual(0)
+})
 
-  it('maps line 0, column N to index N', function() {
-    const map = new LinesAndColumns('abcd');
-    eq(map.indexForLocation({ line: 0, column: 2 }), 2);
-  });
+test('maps line 0, column N to index N', function () {
+  const map = new LinesAndColumns('abcd')
+  expect(map.indexForLocation({ line: 0, column: 2 })).toEqual(2)
+})
 
-  it('maps line/column to the appropriate index', function() {
-    const map = new LinesAndColumns('ab\ncd');
-    eq(map.indexForLocation({ line: 0, column: 2 }), 2);
-    eq(map.indexForLocation({ line: 1, column: 0 }), 3);
-    eq(map.indexForLocation({ line: 1, column: 2 }), 5);
-  });
+test('maps line/column to the appropriate index', function () {
+  const map = new LinesAndColumns('ab\ncd')
+  expect(map.indexForLocation({ line: 0, column: 2 })).toEqual(2)
+  expect(map.indexForLocation({ line: 1, column: 0 })).toEqual(3)
+  expect(map.indexForLocation({ line: 1, column: 2 })).toEqual(5)
+})
 
-  it('maps non-existent line to null', function() {
-    const map = new LinesAndColumns('a\nb');
-    eq(map.indexForLocation({ line: -1, column: 0 }), null);
-    eq(map.indexForLocation({ line: 2, column: 0 }), null);
-  });
+test('maps non-existent line to null', function () {
+  const map = new LinesAndColumns('a\nb')
+  expect(map.indexForLocation({ line: -1, column: 0 })).toEqual(null)
+  expect(map.indexForLocation({ line: 2, column: 0 })).toEqual(null)
+})
 
-  it('maps non-existent columns to null', function() {
-    const map = new LinesAndColumns('a\nb');
-    eq(map.indexForLocation({ line: 0, column: 3 }), null);
-    eq(map.indexForLocation({ line: 0, column: -1 }), null);
-  });
+test('maps non-existent columns to null', function () {
+  const map = new LinesAndColumns('a\nb')
+  expect(map.indexForLocation({ line: 0, column: 3 })).toEqual(null)
+  expect(map.indexForLocation({ line: 0, column: -1 })).toEqual(null)
+})
 
-  it('handles carriage returns as newline characters', function() {
-    const map = new LinesAndColumns('a\rb');
-    deq(map.locationForIndex(2), { line: 1, column: 0 });
-    deq(map.indexForLocation({ line: 1, column: 0 }), 2);
-  });
+test('handles carriage returns as newline characters', function () {
+  const map = new LinesAndColumns('a\rb')
+  expect(map.locationForIndex(2)).toEqual({ line: 1, column: 0 })
+  expect(map.indexForLocation({ line: 1, column: 0 })).toEqual(2)
+})
 
-  it('handles joined carriage return line feeds as newlines', function() {
-    const map = new LinesAndColumns('a\r\nb');
-    deq(map.locationForIndex(0), { line: 0, column: 0 });
-    deq(map.locationForIndex(1), { line: 0, column: 1 });
-    deq(map.locationForIndex(2), { line: 0, column: 2 });
-    deq(map.locationForIndex(3), { line: 1, column: 0 });
-    deq(map.indexForLocation({ line: 0, column: 0 }), 0);
-    deq(map.indexForLocation({ line: 0, column: 1 }), 1);
-    deq(map.indexForLocation({ line: 0, column: 2 }), 2);
-    deq(map.indexForLocation({ line: 1, column: 0 }), 3);
-  });
+test('handles joined carriage return line feeds as newlines', function () {
+  const map = new LinesAndColumns('a\r\nb')
+  expect(map.locationForIndex(0)).toEqual({ line: 0, column: 0 })
+  expect(map.locationForIndex(1)).toEqual({ line: 0, column: 1 })
+  expect(map.locationForIndex(2)).toEqual({ line: 0, column: 2 })
+  expect(map.locationForIndex(3)).toEqual({ line: 1, column: 0 })
+  expect(map.indexForLocation({ line: 0, column: 0 })).toEqual(0)
+  expect(map.indexForLocation({ line: 0, column: 1 })).toEqual(1)
+  expect(map.indexForLocation({ line: 0, column: 2 })).toEqual(2)
+  expect(map.indexForLocation({ line: 1, column: 0 })).toEqual(3)
+})
 
-  it('handles separate carriage returns and line feeds as independent newlines', function() {
-    const map = new LinesAndColumns('a\rb\nc');
-    deq(map.locationForIndex(0), { line: 0, column: 0 });
-    deq(map.locationForIndex(1), { line: 0, column: 1 });
-    deq(map.locationForIndex(2), { line: 1, column: 0 });
-    deq(map.locationForIndex(3), { line: 1, column: 1 });
-    deq(map.locationForIndex(4), { line: 2, column: 0 });
-    deq(map.indexForLocation({ line: 0, column: 0 }), 0);
-    deq(map.indexForLocation({ line: 0, column: 1 }), 1);
-    deq(map.indexForLocation({ line: 1, column: 0 }), 2);
-    deq(map.indexForLocation({ line: 1, column: 1 }), 3);
-    deq(map.indexForLocation({ line: 2, column: 0 }), 4);
-  });
-});
+test('handles separate carriage returns and line feeds as independent newlines', function () {
+  const map = new LinesAndColumns('a\rb\nc')
+  expect(map.locationForIndex(0)).toEqual({ line: 0, column: 0 })
+  expect(map.locationForIndex(1)).toEqual({ line: 0, column: 1 })
+  expect(map.locationForIndex(2)).toEqual({ line: 1, column: 0 })
+  expect(map.locationForIndex(3)).toEqual({ line: 1, column: 1 })
+  expect(map.locationForIndex(4)).toEqual({ line: 2, column: 0 })
+  expect(map.indexForLocation({ line: 0, column: 0 })).toEqual(0)
+  expect(map.indexForLocation({ line: 0, column: 1 })).toEqual(1)
+  expect(map.indexForLocation({ line: 1, column: 0 })).toEqual(2)
+  expect(map.indexForLocation({ line: 1, column: 1 })).toEqual(3)
+  expect(map.indexForLocation({ line: 2, column: 0 })).toEqual(4)
+})
